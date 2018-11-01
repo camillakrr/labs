ed0e8e15d96db098966d2632f1a271b2232670a3
camillakrr <35482058+camillakrr@users.noreply.github.com>
2018-11-01 21:23:32 +0300

    week11

---
+++ week11/ex2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char const *argv[])
+{
+	printf("Hello");
+	sleep(1);
+	printf("Hello");
+	sleep(1);
+	printf("Hello");
+	sleep(1);
+	printf("Hello");
+	sleep(1);
+	printf("Hello");
+	sleep(1);
+	return 0;
+}
